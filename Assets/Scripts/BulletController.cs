b01199881d05fdbea710ed0668152837bf4d2053
George Ayman <90778453+georgeayman97@users.noreply.github.com>
2021-09-16 23:39:14 +0200

    Add files via upload
    initial Commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float moveSpeed, lifeTime;
+
+    public Rigidbody theRB;
+
+    public GameObject impactEffect;
+
+    public int damage = 1;
+
+    public bool damageEnemy, damagePlayer;
+     //int chest = 0, leftLeg = 0, rightLeg = 0, leftHand = 0, rightHand = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.forward * moveSpeed;
+
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" && damageEnemy)
+        { 
+
+             other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage, "Chest");
+
+
+                if (other.gameObject.tag == "Headshot" && damageEnemy)
+                {
+                    other.transform.parent.GetComponent<EnemyHealthController>().DamageEnemy(damage * 2, "Headshot");
+                    Debug.Log("Headshot");
+                }
+                    if (other.gameObject.tag == "Chest" && damageEnemy)
+                {
+                    other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage, "Chest");
+                    Debug.Log("Chest");
+                }
+                if (other.gameObject.tag == "LeftLeg" && damageEnemy)
+                {
+                    other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage, "LeftLeg");
+                    Debug.Log("LeftLeg");
+                }
+                if (other.gameObject.tag == "RightLeg" && damageEnemy)
+                {
+                    other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage, "RightLeg");
+                    Debug.Log("RightLeg");
+                }
+                if (other.gameObject.tag == "LeftHand" && damageEnemy)
+                {
+                    other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage, "LeftHand");
+                    Debug.Log("LeftHand");
+                }
+                if (other.gameObject.tag == "RightHand" && damageEnemy)
+                {
+                    other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage, "RightHand");
+                    Debug.Log("RightHand");
+                }
+       }
+
+        if (other.gameObject.tag == "Player" && damagePlayer)
+        {
+            PlayerHealthController.instance.DamagePlayer(damage);
+        }
+        
+
+        Destroy(gameObject);
+        Instantiate(impactEffect, transform.position + (transform.forward * (-moveSpeed * Time.deltaTime)), transform.rotation);
+        
+        
+
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyHealthController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class EnemyHealthController : MonoBehaviour
+{
+    public int currentHealth = 5;
+
+    public EnemyController theEC;
+    public GameObject head, leftArm, rightArm;
+    int  headShot = 0 , chest = 0, leftLeg = 0, rightLeg = 0, leftHand = 0, rightHand = 0 ;
+    string readText = File.ReadAllText("WriteLines.txt");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageEnemy(int damageAmount , string bodyPoint)
+    {
+        currentHealth -= damageAmount;
+
+
+        if (bodyPoint == "Headshot" )
+        {
+
+            headShot++;
+        }
+        if (bodyPoint == "Chest" )
+        {
+            
+            chest++;
+        }
+        if (bodyPoint == "LeftLeg" )
+        {
+            
+            leftLeg++;
+        }
+        if (bodyPoint == "RightLeg" )
+        {
+            
+            rightLeg++;
+        }
+        if (bodyPoint == "LeftHand")
+        {
+            
+            leftHand++;
+        }
+        if (bodyPoint == "RightHand" )
+        {
+            
+            rightHand++;
+        }
+
+
+        if (theEC != null)
+        {
+            theEC.GetShot();
+        }
+
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
+
+            AudioManager.instance.PlaySFX(2);
+            string[] lines =
+            {
+            "HeadShot : "+headShot.ToString(), "Chest : "+ chest.ToString(), "LeftLeg : "+leftLeg.ToString(), "RightLeg : "+rightLeg.ToString(),"LeftHand : "+leftHand.ToString(),"RightHand : "+rightHand.ToString()
+        };
+
+            File.WriteAllLines("WriteLines.txt", lines);
+        }
+    }
+}
